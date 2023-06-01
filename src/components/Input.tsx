40d3d3844d73edf25e506d19696733c86d6aa8fd
Justin Edwards <justinedwards1230@gmail.com>
2023-06-01 00:00:30 -0400

    feat(Input): add animation to settings dialog

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
@@ -76,66 +76,72 @@ export default function Input({
                     </div>
                 </div>
             </motion.form>
-            {settingsOpen && (
-                <motion.dialog
-                    layout
-                    initial={{
-                        opacity: 0,
-                    }}
-                    animate={{
-                        opacity: 1,
-                    }}
-                    transition={{
-                        duration: 0.2,
-                    }}
-                    className="flex h-screen w-screen items-center justify-center bg-neutral-300/75 dark:bg-neutral-800/75"
-                >
-                    <div className="relative flex min-w-[30%] flex-col rounded border border-neutral-300 bg-neutral-200 px-4 py-4 text-neutral-900 shadow-lg dark:border-neutral-600 dark:bg-neutral-700 dark:text-neutral-100">
-                        <div className="flex justify-center pb-2 text-lg">
-                            Config
+            <AnimatePresence>
+                {settingsOpen && (
+                    <motion.dialog
+                        key="settings-dialog"
+                        layout
+                        initial={{
+                            opacity: 0,
+                        }}
+                        animate={{
+                            opacity: 1,
+                        }}
+                        exit={{
+                            opacity: 0,
+                        }}
+                        transition={{
+                            duration: 0.2,
+                        }}
+                        className="flex h-screen w-screen items-center justify-center bg-neutral-300/75 dark:bg-neutral-800/75"
+                    >
+                        <div className="relative flex min-w-[30%] flex-col rounded border border-neutral-300 bg-neutral-200 px-4 py-4 text-neutral-900 shadow-lg dark:border-neutral-600 dark:bg-neutral-700 dark:text-neutral-100">
+                            <div className="flex justify-center pb-2 text-lg">
+                                Config
+                            </div>
+                            <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
+                                <div>Model:</div>
+                                <select className="bg-inherit">
+                                    <option value="gpt-3.5-turbo">
+                                        gpt-3.5-turbo
+                                    </option>
+                                    <option value="gpt-4">gpt-4</option>
+                                </select>
+                            </div>
+                            <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
+                                <div>OpenAI API Key:</div>
+                                <input
+                                    type="text"
+                                    className="bg-inherit text-right"
+                                    placeholder="************"
+                                />
+                            </div>
+                            <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
+                                <div>Google API Key:</div>
+                                <input
+                                    type="text"
+                                    className="bg-inherit text-right"
+                                    placeholder="************"
+                                />
+                            </div>
+                            <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
+                                <div>Google CSE API Key:</div>
+                                <input
+                                    type="text"
+                                    className="bg-inherit text-right"
+                                    placeholder="************"
+                                />
+                            </div>
+                            <div
+                                className="absolute right-4 top-4 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-neutral-300 text-center hover:bg-neutral-400 dark:bg-neutral-600 dark:hover:bg-neutral-500"
+                                onClick={() => setSettingsOpen(!settingsOpen)}
+                            >
+                                x
+                            </div>
                         </div>
-                        <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
-                            <div>Model:</div>
-                            <select className="bg-inherit">
-                                <option value="gpt-3.5-turbo">
-                                    gpt-3.5-turbo
-                                </option>
-                                <option value="gpt-4">gpt-4</option>
-                            </select>
-                        </div>
-                        <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
-                            <div>OpenAI API Key:</div>
-                            <input
-                                type="text"
-                                className="bg-inherit text-right"
-                                placeholder="************"
-                            />
-                        </div>
-                        <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
-                            <div>Google API Key:</div>
-                            <input
-                                type="text"
-                                className="bg-inherit text-right"
-                                placeholder="************"
-                            />
-                        </div>
-                        <div className="flex w-full items-center justify-between gap-4 px-4 py-2 dark:bg-neutral-800">
-                            <div>Google CSE API Key:</div>
-                            <input
-                                type="text"
-                                className="bg-inherit text-right"
-                                placeholder="************"
-                            />
-                        </div>
-                        <div
-                            className="absolute right-4 top-4 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-neutral-300 text-center hover:bg-neutral-400 dark:bg-neutral-600 dark:hover:bg-neutral-500"
-                            onClick={() => setSettingsOpen(!settingsOpen)}
-                        >
-                            x
-                        </div>
-                    </div>
-                </motion.dialog>
-            )}
+                    </motion.dialog>
+                )}
+            </AnimatePresence>
         </>
     );
 }
