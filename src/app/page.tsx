2ad3e4c6a1a6a593a7f8466eb6aab234df49d622
Justin Edwards <justinedwards1230@gmail.com>
2023-06-03 15:17:53 -0400

    fix: transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export const runtime = 'edge';
 
 export default async function Page({ searchParams }: { searchParams: any }) {
     return (
-        <main className="flex h-full w-full max-w-full flex-col items-center justify-start gap-8 rounded px-2 py-4 transition-all lg:px-8">
+        <main className="flex h-full w-full max-w-full flex-col items-center justify-start gap-8 rounded px-2 py-4 transition-transform lg:px-8">
             <Suspense>
                 <Header search={searchParams.q} />
             </Suspense>
