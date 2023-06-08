f7ef263e7d74b8070d8b6881ee084f1ce367ae72
Justin <justinedwards1230@gmail.com>
2023-06-08 18:24:55 -0400

    fix: chat not remembering user context

--- src/lib/langchain.ts
+++ src/lib/langchain.ts
@@ -3,7 +3,11 @@ import { AIChatMessage, HumanChatMessage } from 'langchain/schema';
 export function resultsToChatMessages(results: Result[]) {
     const messages: (HumanChatMessage | AIChatMessage)[] = [];
     for (const result of results) {
-        messages.push(new HumanChatMessage(result.query));
+        messages.push(
+            new HumanChatMessage(
+                `${result.query}\n\nUser provided context:\n${result.context}`
+            )
+        );
         messages.push(new AIChatMessage(result.summary));
     }
     return messages;
